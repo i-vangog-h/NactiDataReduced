d8f006477bcd75510c8fcbe55ead85ca3e95a6e7
ivangogh <ivan.turko.03@gmail.com>
2024-07-04 00:14:58 +0200

    init commit

---
+++ Operations.cs
@@ -0,0 +1,25 @@
+﻿namespace NactiData.Services;
+
+public static class Operations
+{
+    public static int Add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    public static int Multiply(int a, int b, int c)
+    {
+        return a * b * c;
+    }
+
+    public static double AddDivide(int a, int b, int c)
+    {
+        if (Environment.GetEnvironmentVariable("WHOLE_DIVISION") == "disable")
+        {
+            return (double)(a + b) / c;
+        }
+
+        return (a + b) / c;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using static NactiData.Services.Operations;
+using static NactiData.Display.ConsoleDisplay;
+
+namespace NactiData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           if (args.Count() != 3) {
+               throw new Exception("Incorrect input: exactly 3 numbers are expected.");
+           }
+
+           int[] numbers = new int[3];
+
+           for (int i = 0; i <= 2 ; i++)
+           {
+                try
+                {
+                    numbers[i] = Int32.Parse(args[i]);
+                }
+                catch (OverflowException)
+                {
+                    throw new OverflowException($"The number is too large at position {i}: got \'{args[i]}\', Int32 number is expected");
+                }
+                catch (FormatException)
+                {
+                    throw new FormatException($"Incorrect input at position {i}: got \'{args[i]}\', Int32 number is expected");
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Unknown exception occured");
+                }
+
+            }
+
+           int a = numbers[0];
+           int b = numbers[1];
+           int c = numbers[2];
+
+           Console.WriteLine(Add(a, b, c));
+           Console.WriteLine(Multiply(a, b, c));
+           Console.WriteLine(AddDivide(a, b, c));
+        }
+    }
+}
